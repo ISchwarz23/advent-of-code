ec0b8fe5a72bd70bee7e54e31f18c454261cf220
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-12-04 07:55:08 +0100

    Improve code of day 4

--- src/main/kotlin/aoc2022/Day04.kt
+++ src/main/kotlin/aoc2022/Day04.kt
@@ -3,13 +3,11 @@ package aoc2022
 object Day04 {
 
     fun part1(input: List<String>): Int {
-        return input.map { toRangePair(it) }
-            .count { it.first.contains(it.second) || it.second.contains(it.first) }
+        return input.map { toRangePair(it) }.count { it.first.contains(it.second) || it.second.contains(it.first) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.map { toRangePair(it) }
-            .count { it.first.contains(it.second.first) || it.first.contains(it.second.last) }
+        return input.map { toRangePair(it) }.count { it.first.overlaps(it.second) }
     }
 }
 
@@ -19,10 +17,13 @@ private fun toRangePair(rangePairString: String): Pair<IntRange, IntRange> {
 }
 
 private fun toRange(rangeString: String): IntRange {
-    val rangePair = rangeString.split("-")
-    return rangePair[0].toInt()..rangePair[1].toInt()
+    val rangeParts = rangeString.split("-")
+    return rangeParts[0].toInt()..rangeParts[1].toInt()
 }
 
 fun IntRange.contains(other: IntRange): Boolean {
     return this.contains(other.first) && this.contains(other.last)
+}
+fun IntRange.overlaps(other: IntRange): Boolean {
+    return this.contains(other.first) || this.contains(other.last)
 }
