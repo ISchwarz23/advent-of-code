f00c0b2883f46825fcd6b004d654394f46c52b36
Ingo Schwarz <ingo.schwarz23@gmail.com>
2024-12-06 23:51:38 +0100

    create solution for day 6 - part 1

--- src/main/kotlin/aoc2024/day06/Day06.kt
+++ src/main/kotlin/aoc2024/day06/Day06.kt
@@ -1,17 +1,53 @@
 package aoc2024.day06
 
+import utils.Vector2
+import utils.indexesOf
+
 /**
  * My solution for day 6 of Advent of Code 2024.
  * The puzzle can be found at the <a href="https://adventofcode.com/2024/day/6">AoC page</a>.
  */
 object Day06 {
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<List<Char>>): Int {
+        var position = input.mapIndexed { y, fields -> Vector2(fields.indexOf('^'), y) }.find { it.x >= 0 }
+            ?: throw RuntimeException("No start symbol in input")
+        var movementDirection = movementDirections[0]
+
+        val visitedPositions = mutableSetOf<Vector2>()
+
+        while (position.y in input.indices && position.x in input[0].indices) {
+            visitedPositions += position
+            position += movementDirection
+
+            if (position.y in input.indices && position.x in input[0].indices && input[position.y.toInt()][position.x.toInt()] == '#') {
+                position -= movementDirection
+                movementDirection = turnRight(movementDirection)
+            }
+        }
+
+        return visitedPositions.size
+    }
+
+    fun part2(input: List<List<Char>>): Int {
         return 0
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+}
 
+
+private val movementDirections = listOf(
+    Vector2(0, -1),
+    Vector2(1, 0),
+    Vector2(0, 1),
+    Vector2(-1, 0),
+)
+
+private fun turnRight(currentDirection: Vector2): Vector2 {
+    var index = movementDirections.indexOf(currentDirection)
+    index++
+    if (index >= movementDirections.size) {
+        index = 0
+    }
+    return movementDirections[index]
 }
---
+++ src/test/kotlin/aoc2024/Day06Test.kt
@@ -0,0 +1,48 @@
+package aoc2024
+
+import aoc2024.day06.Day06
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.TestMethodOrder
+import utils.aocClient
+import utils.readInput
+import utils.readInputAsChars
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@TestMethodOrder(
+    MethodOrderer.Alphanumeric::class
+)
+internal class Day06Test {
+
+    private val input = readInputAsChars("aoc2024/day06.txt")
+    private val inputExample = readInputAsChars("aoc2024/day06_example.txt")
+
+    @Test
+    internal fun testPart1() {
+        // when
+        val result = Day06.part1(inputExample)
+
+        // then
+        assertEquals(41, result)
+
+        // submit answer
+        val answer = Day06.part1(input)
+        val submissionResult = aocClient.submit(2024, 6, 1, answer)
+        println("Result of Day 06 - Part 1: $answer (Submission: $submissionResult)")
+    }
+
+    @Test
+    internal fun testPart2() {
+        // when
+        val result = Day06.part2(inputExample)
+
+        // then
+        assertEquals(6, result)
+
+        // submit answer
+        val answer = Day06.part2(input)
+        val submissionResult = aocClient.submit(2024, 6, 2, answer)
+        println("Result of Day 06 - Part 2: $answer (Submission: $submissionResult)")
+    }
+
+}
