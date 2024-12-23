b6313efdb38f3db44d02bdad08bd1a142f40278e
Ingo Schwarz <ingo.schwarz23@gmail.com>
2024-12-23 13:58:45 +0100

    implement solution of day 11 - part 1 (2023)

---
+++ src/main/kotlin/aoc2023/day11/Day11.kt
@@ -0,0 +1,41 @@
+package aoc2023.day11
+
+import utils.Vector2
+
+/**
+ * My solution for day 11 of Advent of Code 2023.
+ * The puzzle can be found at the <a href="https://adventofcode.com/2023/day/11">AoC page</a>.
+ */
+object Day11 {
+
+    fun part1(input: List<List<Char>>): Long {
+        val rawGalaxyCoordinates = input.flatMapIndexed { y: Int, row: List<Char> ->
+            row.mapIndexed { x, c -> if(c == '#') Vector2(x, y) else null }
+        }.filterNotNull()
+
+        val columnsWithoutGalaxies = MutableList(input[0].size) { it }
+        val rowsWithoutGalaxies = MutableList(input.size) { it }
+        rawGalaxyCoordinates.forEach {
+            columnsWithoutGalaxies -= it.x.toInt()
+            rowsWithoutGalaxies -= it.y.toInt()
+        }
+
+        val galaxyCoordinates = rawGalaxyCoordinates.map { rawGalaxyCoordinate -> Vector2(
+            rawGalaxyCoordinate.x + columnsWithoutGalaxies.count { it < rawGalaxyCoordinate.x },
+            rawGalaxyCoordinate.y + rowsWithoutGalaxies.count { it < rawGalaxyCoordinate.y }
+        ) }
+
+        var distanceSum = 0L
+        galaxyCoordinates.subList(0, galaxyCoordinates.size - 1).forEachIndexed { firstGalaxyIndex, firstGalaxy ->
+            galaxyCoordinates.subList(firstGalaxyIndex + 1, galaxyCoordinates.size).forEach { secondsGalaxy ->
+                distanceSum += firstGalaxy.manhattanDistanceTo(secondsGalaxy)
+            }
+        }
+        return distanceSum
+    }
+
+    fun part2(input: List<List<Char>>): Int {
+        return 0
+    }
+
+}
---
+++ src/test/kotlin/aoc2023/Day11Test.kt
@@ -0,0 +1,48 @@
+package aoc2023
+
+import aoc2023.day11.Day11
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.TestMethodOrder
+import utils.aocClient
+import utils.readInput
+import utils.readInputAsChars
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@TestMethodOrder(
+    MethodOrderer.Alphanumeric::class
+)
+internal class Day11Test {
+
+    private val input = readInputAsChars("aoc2023/day11.txt")
+    private val inputExample = readInputAsChars("aoc2023/day11_example.txt")
+
+    @Test
+    internal fun testPart1() {
+        // when
+        val result = Day11.part1(inputExample)
+
+        // then
+        assertEquals(374, result)
+
+        // submit answer
+        val answer = Day11.part1(input)
+        val submissionResult = aocClient.submit(2023, 11, 1, answer)
+        println("Result of Day 11 - Part 1: $answer (Submission: $submissionResult)")
+    }
+
+    @Test
+    internal fun testPart2() {
+        // when
+        val result = Day11.part2(inputExample)
+
+        // then
+        assertEquals(-1, result)
+
+        // submit answer
+        val answer = Day11.part2(input)
+        val submissionResult = aocClient.submit(2023, 11, 2, answer)
+        println("Result of Day 11 - Part 2: $answer (Submission: $submissionResult)")
+    }
+
+}
